00d78cd83cc13cfc492d26c7c4a50ae1d9fa8b96
y4shiro <y4shironao@gmail.com>
2021-11-19 11:13:56 +0900

    4.1.3 call, apply, bind

--- src/chapter04.ts
+++ src/chapter04.ts
@@ -1,3 +1,4 @@
+// 4.1.2
 function sum(numbers: number[]): number {
   return numbers.reduce((total, n) => total + n, 0);
 }
@@ -17,3 +18,13 @@ sumVariabdicSafe(1, 2, 3);
 interface Console {
   log(message?: any, ...optinalParams: any[]): void;
 }
+
+// 4.1.3
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+add(10, 20);
+add.apply(null, [10, 20]);
+add.call(null, 10, 20);
+add.bind(null, 10, 20)();
