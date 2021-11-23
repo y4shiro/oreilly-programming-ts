82f162e0d3835ce1f3837d4bb183483fcfafe0f8
y4shiro <y4shironao@gmail.com>
2021-11-23 17:22:21 +0900

    5.3 戻り値の型として this を使う

--- src/chapter05.ts
+++ src/chapter05.ts
@@ -1,3 +1,4 @@
+// 5.1 クラスと継承
 type Color = 'Black' | 'White';
 type ChessFile = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
 type ChessRank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
@@ -12,13 +13,13 @@ class Game {
       new King('White', 'E', 1),
       new King('Black', 'E', 8),
       // クイーン
-      new Queen('White', 'D', 1),
-      new Queen('Black', 'D', 8),
+      // new Queen('White', 'D', 1),
+      // new Queen('Black', 'D', 8),
       // ビショップ
-      new Bishop('White', 'C', 1),
-      new Bishop('White', 'F', 1),
-      new Bishop('Black', 'C', 8),
-      new Bishop('Black', 'F', 8),
+      // new Bishop('White', 'C', 1),
+      // new Bishop('White', 'F', 1),
+      // new Bishop('Black', 'C', 8),
+      // new Bishop('Black', 'F', 8),
     ];
   }
 }
@@ -57,8 +58,24 @@ class King extends Piece {
   }
 }
 
-class Queen extends Piece {} // クイーン
-class Bishop extends Piece {} // ビショップ
-class Knight extends Piece {} // ナイト
-class Rook extends Piece {} // ルーク
-class Pawn extends Piece {} // ポーン
+// class Queen extends Piece {} // クイーン
+// class Bishop extends Piece {} // ビショップ
+// class Knight extends Piece {} // ナイト
+// class Rook extends Piece {} // ルーク
+// class Pawn extends Piece {} // ポーン
+
+// 5.3
+// const set = new Set();
+// set.add(1).add(2).add(3);
+// set.has(2); // true
+// set.has(4); // false
+
+class Set {
+  has(value: number): boolean {}
+  add(value: number): this {}
+}
+
+class MutableSet extends Set {
+  delete(value: number): boolean {}
+  add(value: number): MutableSet {}
+}
