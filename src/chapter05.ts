fcf96651427a418332bf56b808dd11b4b18d53f1
y4shiro <y4shironao@gmail.com>
2021-11-23 17:08:02 +0900

    5.1 クラスと継承

---
+++ src/chapter05.ts
@@ -0,0 +1,64 @@
+type Color = 'Black' | 'White';
+type ChessFile = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
+type ChessRank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+// チェスのゲームを表す
+class Game {
+  private pieces = Game.makePieces();
+
+  private static makePieces() {
+    return [
+      // キング
+      new King('White', 'E', 1),
+      new King('Black', 'E', 8),
+      // クイーン
+      new Queen('White', 'D', 1),
+      new Queen('Black', 'D', 8),
+      // ビショップ
+      new Bishop('White', 'C', 1),
+      new Bishop('White', 'F', 1),
+      new Bishop('Black', 'C', 8),
+      new Bishop('Black', 'F', 8),
+    ];
+  }
+}
+
+// 駒の座標
+class Position {
+  constructor(private file: ChessFile, private rank: ChessRank) {}
+
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    };
+  }
+}
+
+// チェスの駒
+abstract class Piece {
+  protected position: Position;
+  constructor(private readonly color: Color, file: ChessFile, rank: ChessRank) {
+    this.position = new Position(file, rank);
+  }
+
+  moveTo(position: Position) {
+    this.position = position;
+  }
+
+  abstract canMoveTo(position: Position): boolean;
+}
+
+// チェスの駒 6 種
+class King extends Piece {
+  canMoveTo(position: Position) {
+    const distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
+
+class Queen extends Piece {} // クイーン
+class Bishop extends Piece {} // ビショップ
+class Knight extends Piece {} // ナイト
+class Rook extends Piece {} // ルーク
+class Pawn extends Piece {} // ポーン
