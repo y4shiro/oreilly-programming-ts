492a500b122142ffc52b1484e442e85e4f94426f
y4shiro <y4shironao@gmail.com>
2021-11-24 15:04:29 +0900

    5.7 ポリモーフィズム

--- src/chapter05.ts
+++ src/chapter05.ts
@@ -211,3 +211,22 @@ class StringDatabase {
     return db;
   }
 }
+
+// 5.7
+// class MyMap<K, V> {
+//   constructor(initialKey: K, initialValue: V) {}
+//   get(key: K): V {}
+//   set(key: K, value: V): void {}
+//   merge<K1, V1>(map: MyMap<K1, V1>): MyMap<K | K1, V | V1> {}
+//   static of<K, V>(k: K, v: V): MyMap<K, V> {}
+// }
+
+// interface MyMap<K, V> {
+//   get(key: K): V;
+//   set(key: K, value: V): void;
+// }
+
+// cosnt a = MyMap<string, number>('k', 1) // MyMap<string, number>
+// cosnt b = MyMap('k', true) // MyMap<string, boolean>
+// a.get('k')
+// b.toLocaleString('k', false)
