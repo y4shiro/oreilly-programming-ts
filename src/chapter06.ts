ec02bcc6c7a14e83b726dd1a4d9c05cdaf35931f
y4shiro <y4shironao@gmail.com>
2021-11-24 20:42:46 +0900

    6.1.2.2 関数の変性

--- src/chapter06.ts
+++ src/chapter06.ts
@@ -1,33 +1,80 @@
 // 6.1.2.1
 
-// サーバーに保存済みの既存ユーザ
-type ExistingUser = {
-  id: number;
-  name: string;
-};
+// // サーバーに保存済みの既存ユーザ
+// type ExistingUser = {
+//   id: number;
+//   name: string;
+// };
 
-//  サーバに保存していない新規ユーザ
-type NewUser = {
-  name: string;
-};
+// //  サーバに保存していない新規ユーザ
+// type NewUser = {
+//   name: string;
+// };
 
-function deleteUser(user: { id?: number; name: string }) {
-  delete user.id;
+// function deleteUser(user: { id?: number; name: string }) {
+//   delete user.id;
+// }
+
+// const existingUser: ExistingUser = {
+//   id: 123456,
+//   name: 'Im a User',
+// };
+
+// deleteUser(existingUser);
+
+// type LegacyUser = {
+//   id?: number | string;
+//   name: string;
+// };
+// const legacyUser: LegacyUser = {
+//   id: '123456',
+//   name: 'Taro',
+// };
+// deleteUser(legacyUser);
+
+// 6.1.2.2
+class Animal {}
+class Bird extends Animal {
+  chirp() {
+    console.log('ぐえー');
+  }
+}
+class Crow extends Bird {
+  caw() {
+    console.log('caw');
+  }
 }
 
-const existingUser: ExistingUser = {
-  id: 123456,
-  name: 'Im a User',
-};
+function chirp(bird: Bird): Bird {
+  bird.chirp();
+  return bird;
+}
 
-deleteUser(existingUser);
+// chirp(new Animal()); // エラー
+chirp(new Bird());
+chirp(new Crow());
 
-type LegacyUser = {
-  id?: number | string;
-  name: string;
-};
-const legacyUser: LegacyUser = {
-  id: '123456',
-  name: 'Taro',
-};
-deleteUser(legacyUser);
+// function clone(f: (b: Bird) => Bird): void {}
+function clone(f: (b: Bird) => Bird): void {
+  const parent = new Bird();
+  const babyBird = f(parent);
+  babyBird.chirp();
+}
+
+// function birdToBird(b: Bird): Bird {}
+// clone(birdToBird);
+
+// function birdToCrow(d: Bird): Crow {}
+// clone(birdToCrow);
+
+// function birdToAnimal(d: Bird): Animal {}
+// clone(birdToAnimal);
+
+function AnimalToBird(a: Animal): Bird {}
+clone(AnimalToBird);
+
+function CrowToBird(c: Crow): Bird {
+  c.caw();
+  return new Bird();
+}
+clone(CrowToBird);
