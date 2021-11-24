82e9e99092cd8b562bf0980550d8f7a98ac0d219
y4shiro <y4shironao@gmail.com>
2021-11-24 15:53:03 +0900

    5.9 デコレータ

--- src/chapter05.ts
+++ src/chapter05.ts
@@ -242,36 +242,48 @@ class StringDatabase {
 //   };
 // }
 
-type ClassConstructor<T> = new (...args: any[]) => T;
+// type ClassConstructor<T> = new (...args: any[]) => T;
 
-function withEZDebug<
-  C extends ClassConstructor<{
-    getDebugValue(): object;
-  }>
->(Class: C) {
-  return class extends Class {
-    debug() {
-      const Name = this.constructor.name;
-      const value = this.getDebugValue();
-      return Name + '(' + JSON.stringify(value) + ')';
-    }
-  };
-}
+// function withEZDebug<
+//   C extends ClassConstructor<{
+//     getDebugValue(): object;
+//   }>
+// >(Class: C) {
+//   return class extends Class {
+//     debug() {
+//       const Name = this.constructor.name;
+//       const value = this.getDebugValue();
+//       return Name + '(' + JSON.stringify(value) + ')';
+//     }
+//   };
+// }
 
-class HardToDebugUser {
-  constructor(
-    private id: number,
-    private firstName: string,
-    private lastName: string
-  ) {}
-  getDebugValue() {
-    return {
-      id: this.id,
-      name: this.firstName + ' ' + this.lastName,
-    };
+// class HardToDebugUser {
+//   constructor(
+//     private id: number,
+//     private firstName: string,
+//     private lastName: string
+//   ) {}
+//   getDebugValue() {
+//     return {
+//       id: this.id,
+//       name: this.firstName + ' ' + this.lastName,
+//     };
+//   }
+// }
+
+// const User = withEZDebug(HardToDebugUser);
+// const user = new User(3, 'Emma', 'Gluzman');
+// user.debug(); // HardToDebugUser({"id", 3 "name": "Emma Gluzman"})
+
+// 5.9
+// @serializable
+// class APIPayload {
+//   getValue(): Payload {}
+// }
+
+const APIPayload = serializable(
+  class APIPayload {
+    getvalue(): Payload {}
   }
-}
-
-const User = withEZDebug(HardToDebugUser);
-const user = new User(3, 'Emma', 'Gluzman');
-user.debug(); // HardToDebugUser({"id", 3 "name": "Emma Gluzman"})
+);
