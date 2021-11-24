089c8879dae900683eccc95fc6a4769d69dd6bae
y4shiro <y4shironao@gmail.com>
2021-11-24 18:10:52 +0900

    5.13 練習問題

--- src/chapter05.ts
+++ src/chapter05.ts
@@ -329,31 +329,142 @@ class StringDatabase {
 // Shoe.create('boot'); // Shoe
 
 // 5.11.2
-class RequestBuilder {
-  private url: string | null = null;
-  private method: 'get' | 'post' | null = null;
-  private data: object | null = null;
+// class RequestBuilder {
+//   private url: string | null = null;
+//   private method: 'get' | 'post' | null = null;
+//   private data: object | null = null;
 
-  setURL(url: string): this {
-    this.url = url;
-    return this;
-  }
+//   setURL(url: string): this {
+//     this.url = url;
+//     return this;
+//   }
 
-  setMethod(method: 'get' | 'post'): this {
-    this.method = method;
-    return this;
-  }
+//   setMethod(method: 'get' | 'post'): this {
+//     this.method = method;
+//     return this;
+//   }
 
-  setData(data: object): this {
-    this.data = data;
-    return this;
-  }
+//   setData(data: object): this {
+//     this.data = data;
+//     return this;
+//   }
 
-  send() {}
+//   send() {}
+// }
+
+// new RequestBuilder()
+//   .setURL('/users')
+//   .setMethod('get')
+//   .setData({ firstName: 'Anna' })
+//   .send();
+
+// 5.13
+// 問3
+type Shoe = {
+  purpose: string;
+};
+
+class BalletFlat implements Shoe {
+  purpose = 'dancing';
 }
 
-new RequestBuilder()
-  .setURL('/users')
-  .setMethod('get')
-  .setData({ firstName: 'Anna' })
-  .send();
+class Boot implements Shoe {
+  purpose = 'woodcutting';
+}
+
+class Sneaker implements Shoe {
+  purpose = 'walking';
+}
+
+type ShoeCreator = {
+  create(type: 'balletFlat'): BalletFlat;
+  create(type: 'boot'): Boot;
+  create(type: 'sneaker'): Sneaker;
+};
+
+const Shoe: ShoeCreator = {
+  create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+    switch (type) {
+      case 'balletFlat':
+        return new BalletFlat();
+      case 'boot':
+        return new Boot();
+      case 'sneaker':
+        return new Sneaker();
+    }
+  },
+};
+
+const boo = Shoe.create('boot'); // Boot
+const bal = Shoe.create('balletFlat'); // balletFlat
+const sn = Shoe.create('sneaker'); // Sneaker
+
+// 問4 a.
+// class RequestBuilder {
+//   protected data: object | null = null;
+//   protected method: 'get' | 'post' | null = null;
+//   protected url: string | null = null;
+
+//   setMethod(method: 'get' | 'post'): RequestBuilderWithMethod {
+//     return new RequestBuilderWithMethod().setMethod(method).setData(this.data);
+//   }
+//   setData(data: object | null): this {
+//     this.data = data;
+//     return this;
+//   }
+// }
+
+// class RequestBuilderWithMethod extends RequestBuilder {
+//   setMethod(method: 'get' | 'post' | null): this {
+//     this.method = method;
+//     return this;
+//   }
+//   setURL(url: string): RequestBuilderWithMethodAndURL {
+//     return new RequestBuilderWithMethodAndURL()
+//       .setMethod(this.method)
+//       .setURL(url)
+//       .setData(this.data);
+//   }
+// }
+
+// class RequestBuilderWithMethodAndURL extends RequestBuilderWithMethod {
+//   setURL(url: string): this {
+//     this.url = url;
+//     return this;
+//   }
+//   send() {
+//     // ...
+//   }
+// }
+
+// new RequestBuilder().setMethod('get').setData({}).setURL('/users').send();
+
+// 問4 b.
+interface BuildableRequest {
+  data?: object;
+  method: 'get' | 'post';
+  url: string;
+}
+class RequestBuilder2 {
+  data?: object;
+  method?: 'get' | 'post';
+  url?: string;
+
+  setData(data: object): this & Pick<BuildableRequest, 'data'> {
+    return Object.assign(this, { data });
+  }
+
+  setMethod(method: 'get' | 'post'): this & Pick<BuildableRequest, 'method'> {
+    return Object.assign(this, { method });
+  }
+
+  setURL(url: string): this & Pick<BuildableRequest, 'url'> {
+    return Object.assign(this, { url });
+  }
+
+  build(this: BuildableRequest) {
+    return this;
+  }
+}
+
+new RequestBuilder2().setData({}).setMethod('get').setURL('/users').build();
