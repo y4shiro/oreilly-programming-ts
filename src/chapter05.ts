11b99059425ca7463d845ffd2500622e3e15d278
y4shiro <y4shironao@gmail.com>
2021-11-24 16:55:24 +0900

    5.11.2 ビルダーパターン

--- src/chapter05.ts
+++ src/chapter05.ts
@@ -297,33 +297,63 @@ class StringDatabase {
 // }
 
 // 5.11.1
-type Shoe = {
-  purpose: string;
-};
+// type Shoe = {
+//   purpose: string;
+// };
 
-class BalletFlat implements Shoe {
-  purpose = 'dancing';
+// class BalletFlat implements Shoe {
+//   purpose = 'dancing';
+// }
+
+// class Boot implements Shoe {
+//   purpose = 'woodcutting';
+// }
+
+// class Sneaker implements Shoe {
+//   purpose = 'walking';
+// }
+
+// const Shoe = {
+//   create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+//     switch (type) {
+//       case 'balletFlat':
+//         return new BalletFlat();
+//       case 'boot':
+//         return new Boot();
+//       case 'sneaker':
+//         return new Sneaker();
+//     }
+//   },
+// };
+
+// Shoe.create('boot'); // Shoe
+
+// 5.11.2
+class RequestBuilder {
+  private url: string | null = null;
+  private method: 'get' | 'post' | null = null;
+  private data: object | null = null;
+
+  setURL(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setData(data: object): this {
+    this.data = data;
+    return this;
+  }
+
+  send() {}
 }
 
-class Boot implements Shoe {
-  purpose = 'woodcutting';
-}
-
-class Sneaker implements Shoe {
-  purpose = 'walking';
-}
-
-const Shoe = {
-  create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
-    switch (type) {
-      case 'balletFlat':
-        return new BalletFlat();
-      case 'boot':
-        return new Boot();
-      case 'sneaker':
-        return new Sneaker();
-    }
-  },
-};
-
-Shoe.create('boot'); // Shoe
+new RequestBuilder()
+  .setURL('/users')
+  .setMethod('get')
+  .setData({ firstName: 'Anna' })
+  .send();
