3e4d59675dad2d16f5147c3a9e80fc4e0022e9fa
y4shiro <y4shironao@gmail.com>
2021-11-25 17:42:07 +0900

    6.3.1.2 keyof 演算子

--- src/chapter06.ts
+++ src/chapter06.ts
@@ -242,6 +242,28 @@ function clone(f: (b: Bird) => Bird): void {
 //     friendList: FriendList;
 //   };
 // };
+// type APIResponse = {
+//   user: {
+//     userId: string;
+//     friendList: {
+//       count: number;
+//       friends: {
+//         firstName: string;
+//         lastName: string;
+//       }[];
+//     };
+//   };
+// };
+
+// type FriendList = APIResponse['user']['friendList'];
+
+// function getAPIResponse(): Promise<APIResponse> {}
+// function renderFriendList(friendList: FriendList) {}
+
+// const response = await getAPIResponse();
+// renderFriendList(response.user.friendList);
+
+// 6.3.1.2
 type APIResponse = {
   user: {
     userId: string;
@@ -255,10 +277,51 @@ type APIResponse = {
   };
 };
 
-type FriendList = APIResponse['user']['friendList'];
+type ResponseKeys = keyof APIResponse; // 'user'
+type UserKeys = keyof APIResponse['user']; // 'userId' | 'friendList
+type FriendListKeys = keyof APIResponse['user']['friendList']; // 'count' | 'friends';
 
-function getAPIResponse(): Promise<APIResponse> {}
-function renderFriendList(friendList: FriendList) {}
+function get<O extends object, K extends keyof O>(o: O, k: K): O[K] {
+  return o[k];
+}
 
-const response = await getAPIResponse();
-renderFriendList(response.user.friendList);
+type ActiveLog = {
+  lastEvent: Date;
+  events: {
+    id: string;
+    timestamp: Date;
+    type: 'Read' | 'Write';
+  }[];
+};
+
+const activityLog: ActiveLog;
+const lastEvent = get(activityLog, 'lastEvent'); // Date
+
+type Get = {
+  <O extends object, K1 extends keyof O>(o: O, k1: K1): O[K1];
+  <O extends object, K1 extends keyof O, K2 extends keyof O[K1]>(
+    o: O,
+    k1: K1,
+    k2: K2
+  ): O[K1][K2];
+  <
+    O extends object,
+    K1 extends keyof O,
+    K2 extends keyof O[K1],
+    K3 extends keyof O[K1][K2]
+  >(
+    o: O,
+    k1: K1,
+    k2: K2,
+    k3: K3
+  ): O[K1][K2][K3];
+};
+
+let get: Get = (object: any, ...keys: string[]) => {
+  let result = object;
+  keys.forEach((k) => (result = result[k]));
+  return result;
+};
+
+get(activityLog, 'events', 0, 'type'); // 'Read' | 'Write'
+get(activityLog, 'bad'); // エラー
