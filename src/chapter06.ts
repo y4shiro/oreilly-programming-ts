cbc15000ea26105f240c61654ce3dc5fefed1682
y4shiro <y4shironao@gmail.com>
2021-11-25 17:56:05 +0900

    6.3.2 レコード型

--- src/chapter06.ts
+++ src/chapter06.ts
@@ -264,64 +264,76 @@ function clone(f: (b: Bird) => Bird): void {
 // renderFriendList(response.user.friendList);
 
 // 6.3.1.2
-type APIResponse = {
-  user: {
-    userId: string;
-    friendList: {
-      count: number;
-      friends: {
-        firstName: string;
-        lastName: string;
-      }[];
-    };
-  };
+// type APIResponse = {
+//   user: {
+//     userId: string;
+//     friendList: {
+//       count: number;
+//       friends: {
+//         firstName: string;
+//         lastName: string;
+//       }[];
+//     };
+//   };
+// };
+
+// type ResponseKeys = keyof APIResponse; // 'user'
+// type UserKeys = keyof APIResponse['user']; // 'userId' | 'friendList
+// type FriendListKeys = keyof APIResponse['user']['friendList']; // 'count' | 'friends';
+
+// function get<O extends object, K extends keyof O>(o: O, k: K): O[K] {
+//   return o[k];
+// }
+
+// type ActiveLog = {
+//   lastEvent: Date;
+//   events: {
+//     id: string;
+//     timestamp: Date;
+//     type: 'Read' | 'Write';
+//   }[];
+// };
+
+// const activityLog: ActiveLog;
+// const lastEvent = get(activityLog, 'lastEvent'); // Date
+
+// type Get = {
+//   <O extends object, K1 extends keyof O>(o: O, k1: K1): O[K1];
+//   <O extends object, K1 extends keyof O, K2 extends keyof O[K1]>(
+//     o: O,
+//     k1: K1,
+//     k2: K2
+//   ): O[K1][K2];
+//   <
+//     O extends object,
+//     K1 extends keyof O,
+//     K2 extends keyof O[K1],
+//     K3 extends keyof O[K1][K2]
+//   >(
+//     o: O,
+//     k1: K1,
+//     k2: K2,
+//     k3: K3
+//   ): O[K1][K2][K3];
+// };
+
+// let get: Get = (object: any, ...keys: string[]) => {
+//   let result = object;
+//   keys.forEach((k) => (result = result[k]));
+//   return result;
+// };
+
+// get(activityLog, 'events', 0, 'type'); // 'Read' | 'Write'
+// get(activityLog, 'bad'); // エラー
+
+// 6.3.2
+type Weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
+type Day = Weekday | 'Sat' | 'Sun';
+
+const nextDay: Record<Weekday, Day> = {
+  Mon: 'Tue',
+  Tue: 'Wed',
+  Wed: 'Thu',
+  Thu: 'Fri',
+  Fri: 'Mon',
 };
-
-type ResponseKeys = keyof APIResponse; // 'user'
-type UserKeys = keyof APIResponse['user']; // 'userId' | 'friendList
-type FriendListKeys = keyof APIResponse['user']['friendList']; // 'count' | 'friends';
-
-function get<O extends object, K extends keyof O>(o: O, k: K): O[K] {
-  return o[k];
-}
-
-type ActiveLog = {
-  lastEvent: Date;
-  events: {
-    id: string;
-    timestamp: Date;
-    type: 'Read' | 'Write';
-  }[];
-};
-
-const activityLog: ActiveLog;
-const lastEvent = get(activityLog, 'lastEvent'); // Date
-
-type Get = {
-  <O extends object, K1 extends keyof O>(o: O, k1: K1): O[K1];
-  <O extends object, K1 extends keyof O, K2 extends keyof O[K1]>(
-    o: O,
-    k1: K1,
-    k2: K2
-  ): O[K1][K2];
-  <
-    O extends object,
-    K1 extends keyof O,
-    K2 extends keyof O[K1],
-    K3 extends keyof O[K1][K2]
-  >(
-    o: O,
-    k1: K1,
-    k2: K2,
-    k3: K3
-  ): O[K1][K2][K3];
-};
-
-let get: Get = (object: any, ...keys: string[]) => {
-  let result = object;
-  keys.forEach((k) => (result = result[k]));
-  return result;
-};
-
-get(activityLog, 'events', 0, 'type'); // 'Read' | 'Write'
-get(activityLog, 'bad'); // エラー
