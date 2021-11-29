3d86fcf7c29e0531cd7a0f6dfb2fa879c2de28a9
y4shiro <y4shironao@gmail.com>
2021-11-29 14:51:20 +0900

    8.2 コールバックの処理

--- src/chapter08.ts
+++ src/chapter08.ts
@@ -1,5 +1,31 @@
 // 8.1 JS のイベントループ
 
-setTimeout(() => console.info('A'), 1);
-setTimeout(() => console.info('B'), 2);
-console.info('C');
+// setTimeout(() => console.info('A'), 1);
+// setTimeout(() => console.info('B'), 2);
+// console.info('C');
+
+import * as fs from 'fs';
+
+// Apache サーバのアクセスログからデータを読み込む
+fs.readFile(
+  '/var/log/apache2/access_log',
+  { encoding: 'utf8' },
+  (error, data) => {
+    if (error) {
+      console.error('error reading!', error);
+      return;
+    }
+    console.info('success reading!', data);
+  }
+);
+
+// 同時に、同じアクセスログにデータを書き込む
+fs.appendFile(
+  '/var/log/apache2/access_log',
+  'New access log entry',
+  (error) => {
+    if (error) {
+      console.error('error writing!', error);
+    }
+  }
+);
