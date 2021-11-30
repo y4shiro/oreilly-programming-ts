3686faca5d47e680a881707c6c6230df025c1eb0
y4shiro <y4shironao@gmail.com>
2021-11-30 18:37:46 +0900

    9.2 型安全な API

--- src/chapter09.ts
+++ src/chapter09.ts
@@ -1,18 +1,38 @@
-// グローバルな window オブジェクトからプロパティを読み取る
-const model = {
-  url: window.location.href,
-};
+// // グローバルな window オブジェクトからプロパティを読み取る
+// const model = {
+//   url: window.location.href,
+// };
 
-// <input /> 要素を作成する
-const input = document.createElement('input');
+// // <input /> 要素を作成する
+// const input = document.createElement('input');
 
-// それに CSS を与える
-input.classList.add('Input', 'URLInput');
+// // それに CSS を与える
+// input.classList.add('Input', 'URLInput');
 
-// ユーザーが入力したときに、モデルを更新する
-input.addEventListener('change', () => (model.url = input.value.toUpperCase()));
+// // ユーザーが入力したときに、モデルを更新する
+// input.addEventListener('change', () => (model.url = input.value.toUpperCase()));
 
-// <input /> を DOM に挿入する
-document.body.appendChild(input);
+// // <input /> を DOM に挿入する
+// document.body.appendChild(input);
 
-document.querySelector('.Element').value;
+// document.querySelector('.Element').value;
+
+// 9.2 型安全な API
+type Request =
+  | { entity: 'user'; data: User }
+  | { entity: 'location'; data: Location };
+
+// client.ts
+async function get<R extends Request>(entity: R['entity']): Promise<R['data']> {
+  const res = await fetch(`/api/${entity}`);
+  const json = await res.json();
+  if (!json) {
+    throw ReferenceError('Empty response');
+  }
+  return json;
+}
+
+// app.ts
+async function startApp() {
+  const user = await get('user');
+}
