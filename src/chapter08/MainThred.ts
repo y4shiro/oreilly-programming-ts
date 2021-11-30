8bbaa8e664438b3da2cb7e4f83950667feeae9a1
y4shiro <y4shironao@gmail.com>
2021-11-30 17:25:24 +0900

    8.6.2 子プロセス Node.js

--- src/chapter08/MainThred.ts
+++ src/chapter08/MainThred.ts
@@ -25,3 +25,14 @@
 //   addedUserToThread: [ThreadID, UserID];
 //   removedUserFromThread: [ThreadID, UserID];
 // };
+
+// 8.6.2 子プロセス (Node.js)
+import { fork } from 'child_process';
+
+const child = fork('./ChildThread.js');
+
+child.on('message', (data) =>
+  console.info('Child process sent a message', data)
+);
+
+child.send({ type: 'syn', data: [3] });
