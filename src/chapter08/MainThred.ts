2267b33d712522ce4cedbfaae353b29865b1d897
y4shiro <y4shironao@gmail.com>
2021-11-30 17:13:38 +0900

    8.6.1.1 型安全なプロトコル

--- src/chapter08/MainThred.ts
+++ src/chapter08/MainThred.ts
@@ -7,21 +7,21 @@
 
 // worker.postMessage('some data');
 
-type Message = string;
-type ThreadID = number;
-type UserID = number;
-type Participants = UserID[];
+// type Message = string;
+// type ThreadID = number;
+// type UserID = number;
+// type Participants = UserID[];
 
-type Commands = {
-  sendMessageToThred: [ThreadID, Message];
-  createThread: [Participants];
-  addUserToThread: [ThreadID, UserID];
-  removeUserFromThread: [ThreadID, UserID];
-};
+// type Commands = {
+//   sendMessageToThred: [ThreadID, Message];
+//   createThread: [Participants];
+//   addUserToThread: [ThreadID, UserID];
+//   removeUserFromThread: [ThreadID, UserID];
+// };
 
-type Events = {
-  receivedMessage: [ThreadID, UserID, Message];
-  createThread: [ThreadID, Participants];
-  addedUserToThread: [ThreadID, UserID];
-  removedUserFromThread: [ThreadID, UserID];
-};
+// type Events = {
+//   receivedMessage: [ThreadID, UserID, Message];
+//   createThread: [ThreadID, Participants];
+//   addedUserToThread: [ThreadID, UserID];
+//   removedUserFromThread: [ThreadID, UserID];
+// };
--- src/chapter08/WorkderScript.ts
+++ src/chapter08/WorkderScript.ts
@@ -2,30 +2,30 @@
 //   console.log(e.data);
 //   postMessage(`Ack: ${e.data}`);
 // };
-import { EventEmitter } from 'events';
+// import { EventEmitter } from 'events';
 
-interface SafeEmitter<Events extends Record<PropertyKey, unknown[]>> {
-  emit<K extends keyof Events>(channel: K, ...data: Events[K]): boolean;
-  on<K extends keyof Events>(
-    channel: K,
-    listener: (...data: Events[K]) => void
-  ): this;
-  on(channel: never, listener: (...data: unknown[]) => void): this;
-}
+// interface SafeEmitter<Events extends Record<PropertyKey, unknown[]>> {
+//   emit<K extends keyof Events>(channel: K, ...data: Events[K]): boolean;
+//   on<K extends keyof Events>(
+//     channel: K,
+//     listener: (...data: Events[K]) => void
+//   ): this;
+//   on(channel: never, listener: (...data: unknown[]) => void): this;
+// }
 
-type Command =
-  | { type: 'sendMessageToThread'; data: [ThreadID, Message] }
-  | { type: 'createThread'; data: [Participants] }
-  | { type: 'addUserToThread'; data: [ThreadID, UserID] }
-  | { type: 'removeUserFromThread'; data: [ThreadID, UserID] };
+// type Command =
+//   | { type: 'sendMessageToThread'; data: [ThreadID, Message] }
+//   | { type: 'createThread'; data: [Participants] }
+//   | { type: 'addUserToThread'; data: [ThreadID, UserID] }
+//   | { type: 'removeUserFromThread'; data: [ThreadID, UserID] };
 
-onmessage = (e) => processCommandFromMainThread(e.data);
+// onmessage = (e) => processCommandFromMainThread(e.data);
 
-function processCommandFromMainThread(command: Command) {
-  switch (command.type) {
-    case 'sendMessageToThread':
-      const [threadID, message] = command.data;
-      console.log(message);
-    // ...
-  }
-}
+// function processCommandFromMainThread(command: Command) {
+//   switch (command.type) {
+//     case 'sendMessageToThread':
+//       const [threadID, message] = command.data;
+//       console.log(message);
+//     // ...
+//   }
+// }
