9693bcd0003b49289f38ab989fe2728629a74a34
y4shiro <y4shironao@gmail.com>
2021-12-01 11:32:35 +0900

    11.1.3 アンビエントモジュール宣言

--- src/chapter11/types.ts
+++ src/chapter11/types.ts
@@ -11,6 +11,18 @@
 // };
 
 // 11.1.2 アンビエント型宣言
-type ToArray<T> = T extends unknown[] ? T : T[];
+// type ToArray<T> = T extends unknown[] ? T : T[];
 
-function toArray<T>(a: T): ToArray<T> {}
+// function toArray<T>(a: T): ToArray<T> {}
+
+// 11.1.3 アンビエントモジュール宣言
+declare module 'module-name' {
+  export type MyType = number;
+  export type MyDefaultType = { a: string };
+  export let myExport: MyType;
+  let myDefaultExport: MyDefaultType;
+  export default myDefaultExport;
+}
+
+// import ModuleName from 'module-name';
+// ModuleName.a; // string
